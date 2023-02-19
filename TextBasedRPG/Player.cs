a05deaed579c10aa024c23f4a54477372d5c0b2a
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 15:58:34 -0800

    Our player needs an alive property to track if he has health

--- TextBasedRPG/Player.cs
+++ TextBasedRPG/Player.cs
@@ -3,19 +3,21 @@ namespace TextBasedRPG;
 public class Player
 {
     public string Name { get; set; }
-    public FishingSkill Fishing { get; set; }
-    public WoodcuttingSkill Woodcutting { get; set; }
-    public FiremakingSkill Firemaking { get; set; }
-    public CookingSkill Cooking { get; set; }
-    public CombatSkill Combat { get; set; }
-
+    public bool IsAlive { get; set; } = true;
+    public int Health { get; set; } = 100;
+    public int MaxHealth { get; set; } = 100;
+    
+    // Constructor
     public Player(string name)
     {
         Name = name;
-        Fishing = new FishingSkill();
-        Woodcutting = new WoodcuttingSkill();
-        Firemaking = new FiremakingSkill();
-        Cooking = new CookingSkill();
-        Combat = new CombatSkill();
     }
+    
+    // Skill properties
+    public FishingSkill Fishing { get; set; } = new FishingSkill();
+    public WoodcuttingSkill Woodcutting { get; set; } = new WoodcuttingSkill();
+    public FiremakingSkill Firemaking { get; set; } = new FiremakingSkill();
+    public CookingSkill Cooking { get; set; } = new CookingSkill();
+    public CombatSkill Combat { get; set; } = new CombatSkill();
 }
+
