420b4026d7384506886a83186513bbcf02f53a78
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 14:57:26 -0800

    Great! Now we need a class to represent the player.  They should have access to all of these skills

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -65,3 +65,15 @@ public class CombatSkill : ISkill
         // TODO: Add logic for gaining experience in combat skill
     }
 }
+
+public class Player
+{
+    public string Name { get; set; }
+    public int HitPoints { get; set; } = 10;
+
+    public FishingSkill Fishing { get; set; } = new FishingSkill();
+    public WoodcuttingSkill Woodcutting { get; set; } = new WoodcuttingSkill();
+    public FiremakingSkill Firemaking { get; set; } = new FiremakingSkill();
+    public CookingSkill Cooking { get; set; } = new CookingSkill();
+    public CombatSkill Combat { get; set; } = new CombatSkill();
+}
