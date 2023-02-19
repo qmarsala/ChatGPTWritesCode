3f7c4ab25710410f726e15e943ba613cf1ae9eac
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 15:00:47 -0800

    Now lets begin the game loop.  Create a program class that accepts text based input to control our player

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -1,79 +1,49 @@
-﻿public interface ISkill
+﻿partial class Program
 {
-    string Name { get; }
-    int Level { get; set; }
-    int Experience { get; set; }
-    void GainExperience(int amount);
-}
-
-public class FishingSkill : ISkill
-{
-    public string Name { get; } = "Fishing";
-    public int Level { get; set; } = 1;
-    public int Experience { get; set; } = 0;
-
-    public void GainExperience(int amount)
+    static void Main(string[] args)
     {
-        // TODO: Add logic for gaining experience in fishing skill
+        // Create a new player
+        var player = new Player();
+
+        // Main game loop
+        while (true)
+        {
+            // Display prompt for user input
+            Console.WriteLine("Enter a command:");
+
+            // Read user input from console
+            var input = Console.ReadLine().Trim().ToLower();
+
+            // Process user input
+            if (input == "quit")
+            {
+                // Exit the game loop
+                break;
+            }
+            else if (input.StartsWith("fish"))
+            {
+                // TODO: Implement fishing logic
+            }
+            else if (input.StartsWith("chop"))
+            {
+                // TODO: Implement woodcutting logic
+            }
+            else if (input.StartsWith("make fire"))
+            {
+                // TODO: Implement firemaking logic
+            }
+            else if (input.StartsWith("cook"))
+            {
+                // TODO: Implement cooking logic
+            }
+            else if (input.StartsWith("fight"))
+            {
+                // TODO: Implement combat logic
+            }
+            else
+            {
+                Console.WriteLine("Invalid command.");
+            }
+        }
     }
-}
-
-public class WoodcuttingSkill : ISkill
-{
-    public string Name { get; } = "Woodcutting";
-    public int Level { get; set; } = 1;
-    public int Experience { get; set; } = 0;
-
-    public void GainExperience(int amount)
-    {
-        // TODO: Add logic for gaining experience in woodcutting skill
-    }
-}
-
-public class FiremakingSkill : ISkill
-{
-    public string Name { get; } = "Firemaking";
-    public int Level { get; set; } = 1;
-    public int Experience { get; set; } = 0;
-
-    public void GainExperience(int amount)
-    {
-        // TODO: Add logic for gaining experience in firemaking skill
-    }
-}
-
-public class CookingSkill : ISkill
-{
-    public string Name { get; } = "Cooking";
-    public int Level { get; set; } = 1;
-    public int Experience { get; set; } = 0;
-
-    public void GainExperience(int amount)
-    {
-        // TODO: Add logic for gaining experience in cooking skill
-    }
-}
-
-public class CombatSkill : ISkill
-{
-    public string Name { get; } = "Combat";
-    public int Level { get; set; } = 1;
-    public int Experience { get; set; } = 0;
-
-    public void GainExperience(int amount)
-    {
-        // TODO: Add logic for gaining experience in combat skill
-    }
-}
-
-public class Player
-{
-    public string Name { get; set; }
-    public int HitPoints { get; set; } = 10;
-
-    public FishingSkill Fishing { get; set; } = new FishingSkill();
-    public WoodcuttingSkill Woodcutting { get; set; } = new WoodcuttingSkill();
-    public FiremakingSkill Firemaking { get; set; } = new FiremakingSkill();
-    public CookingSkill Cooking { get; set; } = new CookingSkill();
-    public CombatSkill Combat { get; set; } = new CombatSkill();
 }
