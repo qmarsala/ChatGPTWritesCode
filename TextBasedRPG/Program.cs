6dce8307775b82abbe10d9f14bfc5cd6641b58c7
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 14:48:46 -0800

    Lets get started on the game.  We will first need to decide what skills we want to have in the game.  I think we will keep it simple for now.  Lets just have fishing, woodcutting, firemaking, cooking, and combat.  Could you provide an interface for skills?

---
+++ TextBasedRPG/Program.cs
@@ -0,0 +1,19 @@
+﻿public interface ISkill
+{
+    string Name { get; }
+    int Level { get; set; }
+    int Experience { get; set; }
+    void GainExperience(int amount);
+}
+
+public class FishingSkill : ISkill
+{
+    public string Name { get; } = "Fishing";
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+
+    public void GainExperience(int amount)
+    {
+        // TODO: Add logic for gaining experience in fishing skill
+    }
+}
