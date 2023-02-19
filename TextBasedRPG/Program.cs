4ff2c40feb138e5f2d340ef765549194bd8b0c35
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 14:56:00 -0800

    now lets implement the rest of the skills

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -17,3 +17,51 @@ public class FishingSkill : ISkill
         // TODO: Add logic for gaining experience in fishing skill
     }
 }
+
+public class WoodcuttingSkill : ISkill
+{
+    public string Name { get; } = "Woodcutting";
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+
+    public void GainExperience(int amount)
+    {
+        // TODO: Add logic for gaining experience in woodcutting skill
+    }
+}
+
+public class FiremakingSkill : ISkill
+{
+    public string Name { get; } = "Firemaking";
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+
+    public void GainExperience(int amount)
+    {
+        // TODO: Add logic for gaining experience in firemaking skill
+    }
+}
+
+public class CookingSkill : ISkill
+{
+    public string Name { get; } = "Cooking";
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+
+    public void GainExperience(int amount)
+    {
+        // TODO: Add logic for gaining experience in cooking skill
+    }
+}
+
+public class CombatSkill : ISkill
+{
+    public string Name { get; } = "Combat";
+    public int Level { get; set; } = 1;
+    public int Experience { get; set; } = 0;
+
+    public void GainExperience(int amount)
+    {
+        // TODO: Add logic for gaining experience in combat skill
+    }
+}
