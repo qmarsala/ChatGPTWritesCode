eb9bb272fb728b1e7414e4c4734f8ab76b4785b8
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 15:53:10 -0800

    We have three compilation errors.  to fix the first, Player needs a constructor to initialize the name.

--- TextBasedRPG/Player.cs
+++ TextBasedRPG/Player.cs
@@ -3,11 +3,19 @@ namespace TextBasedRPG;
 public class Player
 {
     public string Name { get; set; }
-    public int Health { get; set; } = 10;
+    public int FishingLevel { get; set; }
+    public int WoodcuttingLevel { get; set; }
+    public int FiremakingLevel { get; set; }
+    public int CookingLevel { get; set; }
+    public int CombatLevel { get; set; }
 
-    public FishingSkill Fishing { get; set; } = new FishingSkill();
-    public WoodcuttingSkill Woodcutting { get; set; } = new WoodcuttingSkill();
-    public FiremakingSkill Firemaking { get; set; } = new FiremakingSkill();
-    public CookingSkill Cooking { get; set; } = new CookingSkill();
-    public CombatSkill Combat { get; set; } = new CombatSkill();
+    public Player(string name)
+    {
+        Name = name;
+        FishingLevel = 1;
+        WoodcuttingLevel = 1;
+        FiremakingLevel = 1;
+        CookingLevel = 1;
+        CombatLevel = 1;
+    }
 }
