f7efd8800d7d4e55bd0cec7a4eaf85cbfc622c88
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 09:40:29 -0800

    that looks like a great plan.  Let's start with the IGame implementation, this interface will define the overall game, including the game loop, world generation, player management, and win/lose conditions.

---
+++ IGame.cs
@@ -0,0 +1,28 @@
+namespace TextBasedRPG;
+
+public interface IGame
+{
+    // Start the game loop
+    void Start();
+
+    // End the game loop
+    void End();
+
+    // Get the current game state
+    GameState GetState();
+
+    // Get the current player
+    IPlayer GetPlayer();
+
+    // Get the current world
+    IWorld GetWorld();
+
+    // Check if the game is over (due to win or lose conditions)
+    bool IsGameOver();
+
+    // Check if the player has won the game
+    bool HasPlayerWon();
+
+    // Check if the player has lost the game
+    bool HasPlayerLost();
+}
