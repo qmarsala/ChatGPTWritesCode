98a3aaa177b130549c5b762a74fc2a46ecdf4e45
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 13:08:23 -0800

    Pretty good.  Can you think of any ways we could improve this implementation?

--- ConsoleQuest.GreatEscape/InputHandler.cs
+++ ConsoleQuest.GreatEscape/InputHandler.cs
@@ -30,5 +30,29 @@ namespace ConsoleQuest
                     return Direction.Up; // Default to up direction
             }
         }
+
+        public async Task<Direction> GetDirectionAsync()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            return Direction.Up;
+                        case ConsoleKey.DownArrow:
+                            return Direction.Down;
+                        case ConsoleKey.LeftArrow:
+                            return Direction.Left;
+                        case ConsoleKey.RightArrow:
+                            return Direction.Right;
+                    }
+                }
+                await Task.Delay(50); // Wait for 50 milliseconds before checking again
+            }
+        }
+
     }
 }
