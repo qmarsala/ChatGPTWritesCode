f986ac44c089fac1f8ec5b30bccd06fb511c570c
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 14:08:35 -0800

    our maze generator doesn't work.  Lets start by making no walls

--- ConsoleQuest.GreatEscape/MazeGenerator.cs
+++ ConsoleQuest.GreatEscape/MazeGenerator.cs
@@ -4,80 +4,26 @@ namespace ConsoleQuest
 {
     public class MazeGenerator
     {
-        private readonly int rows;
-        private readonly int cols;
         private readonly Maze maze;
 
-        public MazeGenerator(int rows, int cols)
+        public MazeGenerator(int width, int height)
         {
-            this.rows = rows;
-            this.cols = cols;
-            this.maze = new Maze(rows, cols);
+            maze = new Maze(width, height);
+            maze.Start = (0, 0);
+            maze.End = (width - 1, height - 1);
         }
 
         public Maze GenerateMaze()
         {
-            for (int row = 0; row < rows; row++)
+            for (int i = 0; i < maze.Width; i++)
             {
-                for (int col = 0; col < cols; col++)
+                for (int j = 0; j < maze.Height; j++)
                 {
-                    maze.SetWall(row, col);
+                    maze.Grid[i, j] = false;
                 }
-            }
-
-            int startRow = 0;
-            int startCol = 0;
-            int endRow = rows - 1;
-            int endCol = cols - 1;
-
-            maze.RemoveWall(startRow, startCol);
-            maze.Start = (startRow, startCol);
-            maze.End = (endRow, endCol);
-
-            while (!PathExists(maze, startRow, startCol, endRow, endCol))
-            {
-                int row = new Random().Next(0, rows);
-                int col = new Random().Next(0, cols);
-
-                if ((row == startRow && col == startCol) || (row == endRow && col == endCol))
-                {
-                    continue;
-                }
-
-                maze.RemoveWall(row, col);
             }
 
             return maze;
-        }
-
-        private bool PathExists(Maze maze, int startRow, int startCol, int endRow, int endCol)
-        {
-            bool[,] visited = new bool[rows, cols];
-            return PathExistsHelper(maze, startRow, startCol, endRow, endCol, visited);
-        }
-
-        private bool PathExistsHelper(Maze maze, int row, int col, int endRow, int endCol, bool[,] visited)
-        {
-            if (row < 0 || row >= rows || col < 0 || col >= cols || maze.IsWall(row, col) || visited[row, col])
-            {
-                return false;
-            }
-
-            if (row == endRow && col == endCol)
-            {
-                return true;
-            }
-
-            visited[row, col] = true;
-
-            bool found = PathExistsHelper(maze, row - 1, col, endRow, endCol, visited)
-                || PathExistsHelper(maze, row, col + 1, endRow, endCol, visited)
-                || PathExistsHelper(maze, row + 1, col, endRow, endCol, visited)
-                || PathExistsHelper(maze, row, col - 1, endRow, endCol, visited);
-
-            visited[row, col] = false;
-
-            return found;
         }
     }
 }
