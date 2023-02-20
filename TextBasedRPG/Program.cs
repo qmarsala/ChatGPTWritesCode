f5d2593308d878f3e66224349918f327cce1f0cd
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 22:51:45 -0800

    lets create a player class based on our player interface

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -49,6 +49,46 @@ public class GameLoop
     }
 }
 
+public class Player : IPlayer
+{
+    public string Name { get; }
+    public int Health { get; private set; }
+    public bool IsAlive => Health > 0;
+
+    public IEnumerable<IItem> Inventory => inventory;
+    private List<IItem> inventory = new List<IItem>();
+
+    public Player(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health < 0)
+        {
+            Health = 0;
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        Health += amount;
+    }
+
+    public void AddToInventory(IItem item)
+    {
+        inventory.Add(item);
+    }
+
+    public void RemoveFromInventory(IItem item)
+    {
+        inventory.Remove(item);
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
