bc62412b72608550eca4b4ee57fdb238a1d4ad14
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 22:05:07 -0800

    We should probably have a world to explore.  What would be a good interface for that?

--- TextBasedRPG/Program.cs
+++ TextBasedRPG/Program.cs
@@ -1,6 +1,22 @@
 ﻿
 namespace TextBasedRPG;
 
+public interface IWorld {
+    // generate the world with the given size
+    void GenerateWorld(int width, int height);
+
+    // get the tile at the specified position
+    ITile GetTile(int x, int y);
+
+    // move the player to the specified position
+    void MovePlayerTo(int x, int y);
+
+    // check if the player can move in the specified direction
+    bool CanMovePlayer(Direction direction);
+}
+
+
+
 public interface IItem
 {
     string Name { get; }
