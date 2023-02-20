b343a67061bfaf23f7df895d11235b8ce9cc36d8
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 13:15:25 -0800

    lets update our main method to wait for user input using our new input handlers GetDirectionAsync method

--- ConsoleQuest.GreatEscape/Program.cs
+++ ConsoleQuest.GreatEscape/Program.cs
@@ -4,13 +4,62 @@ namespace ConsoleQuest
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Welcome to Console Quest: The Great Escape!");
-            Console.WriteLine("You find yourself trapped in a mysterious maze and must find your way out to win the game.");
-            Console.WriteLine("Use the arrow keys to move up, down, left, and right.");
+            Console.WriteLine("Use the arrow keys to navigate the maze and find the exit.");
 
-            // TODO: Add game logic here
+            MazeGenerator mazeGenerator = new MazeGenerator();
+            Maze maze = mazeGenerator.GenerateMaze();
+
+            MazeRenderer mazeRenderer = new MazeRenderer(maze);
+            mazeRenderer.Render();
+
+            int currentRow = maze.StartRow;
+            int currentCol = maze.StartCol;
+
+            while (true)
+            {
+                InputHandler inputHandler = new InputHandler();
+                var direction = await inputHandler.GetDirectionAsync();
+                int newRow = currentRow;
+                int newCol = currentCol;
+
+                switch (direction)
+                {
+                    case InputHandler.Direction.Up:
+                        newRow--;
+                        break;
+                    case InputHandler.Direction.Down:
+                        newRow++;
+                        break;
+                    case InputHandler.Direction.Left:
+                        newCol--;
+                        break;
+                    case InputHandler.Direction.Right:
+                        newCol++;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (maze.IsExit(newRow, newCol))
+                {
+                    Console.WriteLine("Congratulations, you escaped the maze!");
+                    break;
+                }
+
+                if (!maze.IsWall(newRow, newCol))
+                {
+                    currentRow = newRow;
+                    currentCol = newCol;
+                }
+
+                mazeRenderer.Render(currentRow, currentCol);
+            }
+
+            Console.ReadLine();
         }
+
     }
 }
