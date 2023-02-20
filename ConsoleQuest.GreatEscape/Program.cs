9bc3b1bbce6ef87003a08aafae84674e9b3346f7
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 13:04:13 -0800

    Lets start with our Main method.  It should introduce the game to the players with a short description and the controls.  We will expect them to use the arrow keys to move around.

---
+++ ConsoleQuest.GreatEscape/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Console Quest: The Great Escape!");
+            Console.WriteLine("You find yourself trapped in a mysterious maze and must find your way out to win the game.");
+            Console.WriteLine("Use the arrow keys to move up, down, left, and right.");
+
+            // TODO: Add game logic here
+        }
+    }
+}
+
