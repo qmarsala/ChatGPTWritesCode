7099d7ec4d95f19bd99a797de10aa7d53bc58d07
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-19 16:16:23 -0800

    our game commands invoke LevelUp on the player skill, but our ISkill lacks that method.  I think instead we should call the GainExperience method we have already defined.  Or do you have a better idea?
    Could you please update the command implementation as discussed?

--- TextBasedRPG/FishingCommand.cs
+++ TextBasedRPG/FishingCommand.cs
@@ -3,9 +3,17 @@ namespace TextBasedRPG;
 public class FishingCommand : ICommand
 {
     public string CommandString => "fish";
-    public string Description => "Catch some fish";
+    public string Description => "Starts fishing.";
+
+    private int xp;
+
+    public FishingCommand(int xp)
+    {
+        this.xp = xp;
+    }
+    
     public void Execute(Player player)
     {
-        player.Fishing.LevelUp();
+        player.Fishing.GainExperience(xp);
     }
 }
