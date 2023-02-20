bce38af83ca85534f453e7e84141d086023da561
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-20 09:57:43 -0800

    human commit: clean up old code. chatgpt and I decided to take a step back

--- TextBasedRPG/IWorld.cs
+++ TextBasedRPG/IWorld.cs
@@ -3,23 +3,14 @@ namespace TextBasedRPG;
 
 public interface IWorld
 {
-    int Height { get; }
     int Width { get; }
+    int Height { get; }
 
-    // generate the world with the given size
+    ITile[,] Tiles { get; }
+
+    ITile GetTileAt(int x, int y);
+    void SetTileAt(int x, int y, ITile tile);
+
     void GenerateWorld();
-
-    // get the tile at the specified position
-    ITile GetTile(int x, int y);
-
-    // move the player to the specified position
-    void MovePlayerTo(int x, int y);
-
-    // check if the player can move in the specified direction
-    bool CanMovePlayer(Direction direction);
-    void MovePlayer(Direction direction);
-    // get a description of the tile at the specified position
-    string GetTileDescription(int x, int y);
-
-    (int x, int y) GetPlayerPosition();
 }
+
