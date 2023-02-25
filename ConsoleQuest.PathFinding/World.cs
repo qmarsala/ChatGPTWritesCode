d6f66af3cd91f8ea3c1a896591c96c3881eecde4
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-25 07:57:17 -0800

    now we need a class to encapsulate the World, which is our collection of nodes

---
+++ ConsoleQuest.PathFinding/World.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleQuest.PathFinding;
+
+public class World<T>
+{
+    private readonly T[,] _nodes;
+
+    public World(T[,] nodes)
+    {
+        _nodes = nodes;
+    }
+
+    public T GetNode(int x, int y)
+    {
+        return _nodes[x, y];
+    }
+
+    public int Width => _nodes.GetLength(0);
+    public int Height => _nodes.GetLength(1);
+}
