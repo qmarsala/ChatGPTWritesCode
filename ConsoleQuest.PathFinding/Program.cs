4962893726c1807945ba44531e5085550d35f55f
Quinten Marsala <quinten.marsala@gmail.com>
2023-02-25 08:02:12 -0800

    Now I think we need to make the example more interesting.  Could you make the obstacles in our world a bit more random?

--- ConsoleQuest.PathFinding/Program.cs
+++ ConsoleQuest.PathFinding/Program.cs
@@ -1,13 +1,32 @@
 ﻿// Create nodes for the graph
 using ConsoleQuest.PathFinding;
 
+int width = 10;
+int height = 10;
 Node[,] graph = new Node[10, 10];
+
 for (int x = 0; x < 10; x++)
 {
     for (int y = 0; y < 10; y++)
     {
         graph[x, y] = new Node(x, y, 1);
+        // Add some random noise to the world
+        Random rand = new Random();
+        int cost = rand.Next(100) < 30 ? 1 : 0;
+        graph[x, y] = new Node(x, y, cost);
     }
+}
+
+// Add some walls around the edge of the world
+for (int x = 0; x < width; x++)
+{
+    graph[x, 0].cost = 1;
+    graph[x, height - 1].cost = 1;
+}
+for (int y = 0; y < height; y++)
+{
+    graph[0, y].cost = 1;
+    graph[width - 1, y].cost = 1;
 }
 
 // Connect nodes in the graph
@@ -35,16 +54,6 @@ for (int x = 0; x < 10; x++)
         }
     }
 }
-
-//
-graph[2, 2].cost = float.PositiveInfinity;
-graph[2, 3].cost = float.PositiveInfinity;
-graph[2, 4].cost = float.PositiveInfinity;
-graph[3, 4].cost = float.PositiveInfinity;
-graph[4, 4].cost = float.PositiveInfinity;
-graph[5, 4].cost = float.PositiveInfinity;
-graph[6, 4].cost = float.PositiveInfinity;
-graph[6, 3].cost = float.PositiveInfinity;
 
 // Find a path using A*
 AStarPathfinder pathfinder = new AStarPathfinder(graph.Cast<Node>().ToList());
